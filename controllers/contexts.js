b801e7db272399f9c61c148c3aeced2acbad3928
JulienAck <dev@webving.fr>
2020-01-08 09:26:02 +0100

    added relations to context edition

--- controllers/contexts.js
+++ controllers/contexts.js
@@ -17,7 +17,7 @@ function list(req, res) {
 function insert(req, res) {
   console.log("contexts::insert");
   dbConnexion.query(
-    "INSERT INTO contexts (id, name, description, image_url) VALUES (nextval(pg_get_serial_sequence('entities', 'id')), $1, $2, $3)",
+    "INSERT INTO contexts (id, name, description, image_url) VALUES (nextval(pg_get_serial_sequence('contexts', 'id')), $1, $2, $3)",
     [req.body.name, req.body.description, req.body.image_url],
     (err, sqlResult) => {
       if (err) throw err;
@@ -29,12 +29,27 @@ function insert(req, res) {
 function edit(req, res) {
   console.log("contexts::edit");
   let id = req.params.id;
-  let sql = "SELECT * FROM contexts WHERE id=$1";
-
-  dbConnexion.query(sql, [id], (err, contexts) => {
+  let sqlContext = "SELECT * FROM contexts WHERE id=$1";
+  let sqlRelations =
+    "SELECT r.id as relation_id, e_source.name as entity_source_name, e_destination.name as entity_destination_name FROM relations r, entities e_source, entities e_destination WHERE e_source.id=r.entity_source_id AND e_destination.id=r.entity_destination_id ORDER BY relation_id DESC";
+  let sqlContextRelationsItems =
+    "SELECT e_destination.name as entity_destination_name, e_source.name as entity_source_name FROM relation_context rc, relations r, entities e_source, entities e_destination WHERE rc.context_id=$1  AND rc.relation_id=r.id AND e_source.id=r.entity_source_id AND e_destination.id=r.entity_destination_id";
+  dbConnexion.query(sqlContext, [id], (err, contextItem) => {
     if (err) throw err;
-    res.render("pages/contextEdit", {
-      contextItem: contexts.rows[0]
+    dbConnexion.query(sqlRelations, (err, relationsItems) => {
+      if (err) throw err;
+      dbConnexion.query(
+        sqlContextRelationsItems,
+        [id],
+        (err, contextRelationsItems) => {
+          if (err) throw err;
+          res.render("pages/contextEdit", {
+            contextItem: contextItem.rows[0],
+            relationsItems: relationsItems.rows,
+            contextRelationsItems: contextRelationsItems.rows
+          });
+        }
+      );
     });
   });
 }
@@ -50,7 +65,7 @@ function update(req, res) {
       req.body.name,
       req.body.description,
       req.body.image_url,
-      req.body.is_live,
+      req.body.is_live
     ],
     (err, sqlResult) => {
       if (err) throw err;
@@ -59,10 +74,23 @@ function update(req, res) {
   );
 }
 
+function insertRelations(req, res) {
+  console.log("contexts::relations::insert");
+  dbConnexion.query(
+    "INSERT INTO relation_context (id, relation_id, context_id) VALUES (nextval(pg_get_serial_sequence('relation_context', 'id')), $1, $2)",
+    [req.body.relation_id, req.body.context_id],
+    (err, sqlResult) => {
+      if (err) throw err;
+      res.redirect("/contexts/edit/" + req.body.context_id);
+    }
+  );
+}
+
 router.get("/", list);
 router.post("/insert", insert);
 router.get("/edit/:id", edit);
 router.post("/update/:id", update);
+router.post("/relations/insert", insertRelations);
 /*
 router.get("/:id", show);
 */
