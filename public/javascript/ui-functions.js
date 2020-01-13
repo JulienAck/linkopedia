30cbc098e7cfe799bdb67c90cc84371308c5b579
JulienAck <dev@webving.fr>
2020-01-13 23:11:38 +0100

    added search into edit entity

--- public/javascript/ui-functions.js
+++ public/javascript/ui-functions.js
@@ -77,40 +77,94 @@ function getAndDrawNetworkFromAPI(apiUrl, containerId) {
     });
 }
 
-function searchFeedDestinationElement(valueItems, searchResultsElementId) {
+// SEARCH
+function searchFeedDestinationElement(
+  valueItems,
+  searchResultsElementId,
+  resultAction
+) {
   var destinationElement = document.getElementById(searchResultsElementId);
   if (destinationElement != undefined) {
-    destinationElement.innerHTML="";
+    destinationElement.innerHTML = "";
     var resultListElements = document.createElement("ul");
     for (let i = 0; i < valueItems.length; i++) {
       var resultItemElement = document.createElement("li");
-      resultItemElement.innerHTML ="<a href='/entities/"+valueItems[i].id+"'><div class='search-result-profile-pic'><img class='' src="+ valueItems[i].profile_pic_url+" /></div>"+valueItems[i].name+"</a>";
+      var resultItemLinkElement = document.createElement("a");
+      resultItemLinkElement.setAttribute(
+        "href",
+        "/entities/" + valueItems[i].id
+      );
+      var resultItemImgContainerElement = document.createElement("div");
+      resultItemImgContainerElement.setAttribute(
+        "class",
+        "search-result-profile-pic"
+      );
+      var resultItemImgElement = document.createElement("img");
+      resultItemImgElement.setAttribute("src", valueItems[i].profile_pic_url);
+      resultItemImgContainerElement.appendChild(resultItemImgElement);
+      resultItemLinkElement.appendChild(resultItemImgContainerElement);
+      resultItemLinkElement.appendChild(
+        document.createTextNode(valueItems[i].name)
+      );
+      resultItemElement.appendChild(resultItemLinkElement);
       resultListElements.appendChild(resultItemElement);
+      if (resultAction == "createInput") {
+        resultItemLinkElement.addEventListener("click", function(event) {
+          event.preventDefault();
+          var entityDestinationIdElement = document.getElementById(
+            "entity-destination-id"
+          );
+          entityDestinationIdElement.value = valueItems[i].id;
+          var entityDestinationIdElement = document.getElementById(
+            "entity-destination-name"
+          );
+          entityDestinationIdElement.innerHTML = valueItems[i].name;
+          var searchEntityAutoCompleteElement = document.getElementById(
+            "searchEntityAutoComplete"
+          );
+          searchEntityAutoCompleteElement.value = "";
+          resultListElements.innerHTML="";
+        });
+      }
     }
     resultListElements.appendChild(document.createElement("br"));
     destinationElement.appendChild(resultListElements);
   }
 }
 
-function searchValueChanged(valueSearched, searchResultsElementId) {
+function searchValueChanged(
+  valueSearched,
+  searchResultsElementId,
+  resultAction
+) {
   if (valueSearched.length > 2) {
+    console.log("Search : " + valueSearched);
     $.ajax({
       url: "/search/entities/" + valueSearched
     })
       .done(function(res) {
-        searchFeedDestinationElement(res, searchResultsElementId);
+        searchFeedDestinationElement(res, searchResultsElementId, resultAction);
       })
       .fail(function(err) {
         console.log("Search error: " + err.status);
       });
+  } else {
+    var destinationElement = document.getElementById(searchResultsElementId);
+    if (destinationElement != undefined) {
+      destinationElement.innerHTML = "";
+    }
   }
 }
 
-function listenSearchAutocompleteQueries(searchResultsElementId) {
-  var searchInputElement = document.getElementById("searchAutoComplete");
+function listenSearchAutocompleteQueries(
+  searchInputElementId,
+  searchResultsElementId,
+  resultAction
+) {
+  var searchInputElement = document.getElementById(searchInputElementId);
   if (searchInputElement != undefined) {
     searchInputElement.addEventListener("input", function(evt) {
-      searchValueChanged(this.value,searchResultsElementId);
+      searchValueChanged(this.value, searchResultsElementId, resultAction);
     });
   }
 }
